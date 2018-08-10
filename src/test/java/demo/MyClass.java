f6540b67a8e74fcadccfbffc35dd25870debf1bb
Mohamed <Mohamed@30.8.68.178>
2018-08-10 15:39:35 +0200

    JUnit Test basic

---
+++ src/test/java/demo/MyClass.java
@@ -0,0 +1,9 @@
+package demo;
+
+public class MyClass {
+	
+	
+	public int multiply(int i, int j){
+		return i*j;
+	}
+}
--- src/test/java/demo/SimpleTest.java
+++ src/test/java/demo/SimpleTest.java
@@ -1,5 +1,18 @@
 package demo;
 
-public class SimpleTest {
 
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class SimpleTest {
+	@Test
+	public void multiplicationParZero(){
+		MyClass tester = new MyClass();
+		
+		
+		assertEquals(0, tester.multiply(10, 0));
+		assertEquals(0, tester.multiply(0, 10));
+		assertEquals(0, tester.multiply(0, 0));
+	}
 }
